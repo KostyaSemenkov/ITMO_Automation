9e26f174198c048889bbf8a92c76251901c588c1
Semenkov_Kostya <koldcoed@mail.ru>
2023-03-15 14:28:50 +0300

    add readme

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -1,19 +1,21 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+def test_page(a, b, c):
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get('https://www.saucedemo.com/')
-user_name = driver.find_elements(By.CSS_SELECTOR, 'user-name')
-pass_word = driver.find_elements(By.CSS_SELECTOR, 'password')
-sub_mit = driver.find_elements(By.CSS_SELECTOR, 'login-button')
-s = [user_name, pass_word, sub_mit]
-count = 0
-for i in range(len(s)):
-    if s[i] is None:
-        continue
+    driver = webdriver.Chrome()
+    driver.get('https://www.saucedemo.com/')
+    user_name = driver.find_elements(By.CSS_SELECTOR, a)
+    pass_word = driver.find_elements(By.CSS_SELECTOR, b)
+    sub_mit = driver.find_elements(By.CSS_SELECTOR, c)
+    s = [a, b, c]
+    count = 0
+    for i in range(len(s)):
+        if s[i] is None:
+            continue
+        else:
+            count += 1
+    if count == 3:
+        return 'Элементы найдены'
     else:
-        count += 1
-if count == 3:
-    print('Элементы найдены')
-else:
-    print('Элементы не найдены')
+        return 'Элементы не найдены'
+print(test_page('user-name', 'password', 'login-button'))
